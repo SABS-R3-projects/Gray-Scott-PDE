3300f780fbce29f166b6470585e82965aac57414
jakke-neiro <jakke.neiro@zoo.ox.ac.uk>
2019-11-26 15:44:20 +0000

    Playing with code...

---
+++ simulation.py
@@ -0,0 +1,11 @@
+from GSPDE import GrayScott
+
+N = 256
+rdSolver = GrayScott(N)
+L = rdSolver.laplacian()
+Du, Dv, F, K = 0.14, 0.06, 0.035, 0.065
+Nt = 320
+
+rdSolver.initialise()
+rdSolver.integrate(Nt, Du, Dv, F, K, L)
+rdSolver.configPlot()
