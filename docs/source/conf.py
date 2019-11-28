e464ce248f1a73a0f9fc4f1fd9668403a3faf768
jakke-neiro <jakke.neiro@zoo.ox.ac.uk>
2019-11-28 00:45:53 +0000

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,19 +10,18 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Gray-Scott-PDE'
+project = 'Gray-Scott PDE'
 copyright = '2019, Augustin, Neiro, van der Plas'
 author = 'Augustin, Neiro, van der Plas'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,10 +30,18 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
