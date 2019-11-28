d8b67d44b64252cdfda5165152822e74c3fa636d
DavAug <david.augustin@gmx.net>
2019-11-28 10:48:58 +0000

    :construction: first init InverseProblem class

---
+++ inverseproblem.py
@@ -0,0 +1,8 @@
+class InverseProblem(object):
+    def __init__(self):
+        # check PINTS how to do this best
+        pass
+
+    def find_parameter(self):
+        # use cma from PINTS
+        pass
