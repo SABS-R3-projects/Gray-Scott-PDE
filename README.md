9465413486de811a12a336f8cc2a945962da06a4
DavAug <david.augustin@gmx.net>
2019-11-28 10:34:06 +0000

    :bug: setup.py and updating README

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-Gray-Scott-PDE
+# Gray-Scott-PDE
+
+Project on solving Gray-Scott forward and inverse problem. For more information on the Gray-Scott equation, please refer to
 
 http://people.maths.ox.ac.uk/trefethen/pdectb/reaction2.pdf
+
+
+## Installation
+
+Clone repository by opening the terminal and typing
+
+```
+git clone https://github.com/SABS-R3-projects/Gray-Scott-PDE/
+```
+
+Then install the grayscott module and all dependencies with
+```
+pip install .
+```
+
+Congratulations! You successfully installed grayscott.
--- setup.py
+++ setup.py
@@ -1,15 +1,60 @@
-from setuptools import setup, find_packages
+import os
+import distutils
+from setuptools import setup, find_packages, Command
+from setuptools.command.build_py import build_py
 
 # Load text for description and license
 with open('README.md') as f:
     readme = f.read()
 
 
+class BuildPyCommand(build_py):
+  """Custom build command."""
+
+  def run(self):
+    self.run_command('pints')
+    build_py.run(self)
+
+
+class PINTS_installer(Command):
+    """A costum command to install PINTS in the process of setting up the PKPD module.
+    """
+    description = ('Clones and pip installs PINTS.')
+
+    def initialize_options(self) -> None:
+        self.commands = []
+
+    def finalize_options(self) -> None:
+        self.commands = [
+            'git clone https://github.com/pints-team/pints.git',
+            'pip install pints/']
+
+    def run(self):
+        """Run commands.
+        """
+        git_clone_cmd = self.commands[0]
+        pip_install_cmd = self.commands[1]
+
+        # clone PINTS repository.
+        if os.path.isdir('pints'):
+            self.announce(
+                'PINTS repository exists in directory',
+                level=distutils.log.INFO)
+        elif not os.path.isfile('./pints'):
+            self.announce(
+                'Running command: %s' % str(git_clone_cmd),
+                level=distutils.log.INFO)
+            os.system(git_clone_cmd)
+
+        # pip install PINTS
+        os.system(pip_install_cmd)
+
+
 # Go!
 setup(
     # Module name (lowercase)
     name='Gray-Scott-PDE',
-    version='0.1dev',
+    version='0.2dev',
 
     # Description
     description='PDE solver and parameter inference for the Gray-Scott model.',
@@ -24,8 +69,11 @@ setup(
     # Packages to include
     packages=find_packages(include=('grayscott',)),
 
-    # install PINTS
-    scripts=['install_pints.py'],
+    # customised commands
+    cmdclass={
+        'pints': PINTS_installer,
+        'build_py': BuildPyCommand
+    },
 
     # List of dependencies
     install_requires=[
