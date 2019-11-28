097eb6054bd5e330a225c65ab92957e57a0d0493
thijs <vdplasthijs@gmail.com>
2019-11-28 18:27:05 +0000

    Commenting

--- Pde_solver.py
+++ Pde_solver.py
@@ -15,18 +15,25 @@ class Solver(pints.ForwardModel):
     ---------------
         n_grid: int
             - Size of x and y dimensions
-        n_time_points: int
+        n_time_points: int, default=8000
             - Number of time points
-        model: str, 'gray-scott' or 'heat'
+        model: str, 'gray-scott' or 'heat', Default='gray-scott'
             - defining model to solve
-
+        n_save_frames: int, Default=100
+            - How many frames (sampled evenly between 0 and n_time_points) to save
+            for e.g. plotting purposes
+        fix_seed: bool, Default=False
+            - If true, the numpy random seed is fixed. This allows users to recreate
+            exactly the same results each time (because initial conditions contain
+            some random noise).
     """
     def __init__(self, n_grid=256, n_time_points=8000, model='gray-scott',
                 n_save_frames=100, fix_seed=False):
         self.solve_eq = model
-        self.eps_1 = 0.14
+        self.eps_1 = 0.14  # hard-coded. These values were found to work well with dx=dy=dt=1
         self.eps_2 = 0.06
 
+        ## Create x and y grids
         self.n_x = n_grid
         self.x_start = 0
         self.x_end = n_grid - 1
@@ -39,7 +46,7 @@ class Solver(pints.ForwardModel):
         self.y_arr = np.linspace(self.y_start, self.y_end, self.n_y)
         self.dy = np.diff(self.y_arr)[0]
 
-        # dt_set = 0.9 * (dx ** 2) / (4 * eps_1[solve_eq])
+        ## Create time array for solving (t_arr) and for saving/plotting (save_frames)
         self.n_times = n_time_points
         self.t_start = 0
         self.t_end = n_time_points - 1
@@ -49,10 +56,10 @@ class Solver(pints.ForwardModel):
         self.save_frames = np.linspace(self.t_start, self.t_end, self.n_save_frames)
         self.save_frames = np.round(self.save_frames)
 
-        ## Set initial conditions:
-        if fix_seed:
+        if fix_seed:  # fix random seed if required
             np.random.seed(0)
 
+        ## Set initial conditions:
         if model == 'gray-scott':
             self.u_mat = np.ones((self.n_x, self.n_y))
         elif model == 'heat':
@@ -60,11 +67,11 @@ class Solver(pints.ForwardModel):
         self.v_mat = np.zeros((self.n_x, self.n_y))
 
         spat_slice = slice(int(7/16 * self.n_x), int(9/16 * self.n_x))   # create square with initial offset
-        self.u_mat[spat_slice, spat_slice] = 0.5
+        self.u_mat[spat_slice, spat_slice] = 0.5  # standard init values for gray scott model
         self.v_mat[spat_slice, spat_slice] = 0.25
         self.u_mat[:, :] += ((np.random.rand(self.n_x, self.n_y) - 0.5) * 0.02)  # add uniform noise
         self.v_mat[:, :] += ((np.random.rand(self.n_x, self.n_y) - 0.5) * 0.02)
-        self.init_u_mat = self.u_mat.copy()
+        self.init_u_mat = self.u_mat.copy()  # save initial values
         self.init_v_mat = self.v_mat.copy()
 
         if model == 'gray-scott':
@@ -92,6 +99,7 @@ class Solver(pints.ForwardModel):
         """
 
         diff = np.zeros_like(old_u)
+        ## Compute central differnece in 2 dimensions:
         diff = diff_coef * (((np.roll(old_u, shift=1, axis=0)
                             + np.roll(old_u, shift=-1, axis=0)
                             - 2 * old_u) / (self.dx ** 2))
@@ -121,10 +129,11 @@ class Solver(pints.ForwardModel):
         new_u_mat = old_u_mat.copy()
         new_v_mat = old_v_mat.copy()
 
+        ## Perform diffusion step:
         new_u_mat += self.dt * self.diffusion_update(old_u=old_u_mat, diff_coef=self.eps_1)
         new_v_mat += self.dt * self.diffusion_update(old_u=old_v_mat, diff_coef=self.eps_2)
 
-        if self.solve_eq == 'gray-scott':
+        if self.solve_eq == 'gray-scott':  ## IF gray=-scott model, perform other two actions:
             if self.interaction:
                 uvv = old_u_mat * np.power(old_v_mat, 2)
                 new_u_mat -= self.dt * uvv
@@ -148,28 +157,34 @@ class Solver(pints.ForwardModel):
                 2nd entry:
                     k: float
                         - parameter of gray-scott model
-
+        Returns:
+        ----------------
+            save_u_mat: float of len (n_save_frames * n_x * n_y)
+                - Returns all save u matrices, collapsed to 1 dimension
         """
 
         assert len(parameters) == 2
-        self.F = parameters[0]
+        self.F = parameters[0]  # find F and k from input
         self.k = parameters[1]
 
         print(f'Solving {self.solve_eq} model in {self.n_times} time steps.\n\n')
 
+        ## Create matrices to save frames during solving at regular intervals
         self.save_u_mat = np.zeros((self.n_save_frames, self.n_x, self.n_y))
         self.save_v_mat = np.zeros((self.n_save_frames, self.n_x, self.n_y))
         self.save_times = np.zeros(self.n_save_frames)
         i_save = 0
+
+        ## Forward difference time solving loop:
         for i_tau in tqdm(range(self.n_times)):
-            if i_tau in self.save_frames:
+            if i_tau in self.save_frames:  # if at the save interval, save matrices
                 self.save_u_mat[i_save, :, :] = self.u_mat.copy()
                 self.save_v_mat[i_save, :, :] = self.v_mat.copy()
                 self.save_times[i_save] = i_tau
                 i_save += 1
             old_u = self.u_mat.copy()
             old_v = self.v_mat.copy()
-            self.u_mat, self.v_mat = self.update_uv(old_u_mat=old_u, old_v_mat=old_v)
+            self.u_mat, self.v_mat = self.update_uv(old_u_mat=old_u, old_v_mat=old_v)  # do update
         self.save_u_mat[-1, :, :] = self.u_mat.copy()
         self.save_v_mat[-1, :, :] = self.v_mat.copy()
 
@@ -183,13 +198,13 @@ class Solver(pints.ForwardModel):
             save_figures: bool, default = False
                 - If true, save figure as png file
         """
-        filename_uv = f'u_matrix_F={self.F}_k={self.k}.png'
+        filename_uv = f'u_matrix_F={self.F}_k={self.k}.png'  # define file name to save to
         plt.rcParams['figure.figsize'] = (12, 5)
-        plt.subplot(121)
+        plt.subplot(121)  # plot u matrix
         plt.pcolor(self.u_mat, cmap=plt.cm.RdBu)
         plt.xlabel('u_1'); plt.ylabel('u_2'); plt.title(f'u matrix, F={self.F}, k={self.k} after {self.n_times} iterations')
         plt.colorbar()
-        plt.subplot(122)
+        plt.subplot(122) # plot v matrix
         plt.pcolor(self.v_mat, cmap=plt.cm.RdBu)
         plt.xlabel('v_1'); plt.ylabel('v_2'); plt.title(f'v matrix, F={self.F}, k={self.k} after {self.n_times} iterations')
         plt.colorbar()
@@ -209,6 +224,7 @@ class Solver(pints.ForwardModel):
         plab.pcolormesh(self.u_mat, cmap=plab.cm.RdBu)
 
         def animate(i):
+            """Plotting function for animation"""
             if i < self.save_u_mat.shape[0]:
                 plab.pcolormesh(np.squeeze(self.save_u_mat[i, :, :]), cmap=plab.cm.RdBu)
 
@@ -222,9 +238,9 @@ class Solver(pints.ForwardModel):
             anim.save(filename_an, writer=writer)
 
     def n_outputs(self):
-        """Return number of outputs."""
+        """Returns number of outputs."""
         return (self.n_x * self.n_y)
 
     def n_parameters(self):
-        """Return number of parameters for inference (F and K)"""
+        """Returns number of parameters for inference (F and K)"""
         return 2
