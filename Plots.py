c5ee237921f1d3edb81227d577e3cc9163678afa
jakke-neiro <jakke.neiro@zoo.ox.ac.uk>
2019-11-28 14:24:07 +0000

    setup.py

--- Plots.py
+++ Plots.py
@@ -17,7 +17,6 @@ class Plot():
 
     def animation(self, values, filename_u):
         values_u = values[0][0: self.N * self.N]
-
         fig = plab.figure()
         plab.pcolormesh(values_u.reshape((self.N, self.N)), cmap=plab.cm.RdBu)
 
@@ -25,6 +24,6 @@ class Plot():
             values_u = values[i][0: self.N * self.N]
             plab.pcolormesh(values_u.reshape((self.N, self.N)), cmap=plab.cm.RdBu)
 
-        anim = animation.FuncAnimation(fig, animate, frames=range(1, values.shape[0]), blit=False)
+        anim = animation.FuncAnimation(fig, animate, frames=range(1, values.shape[0], 10), blit=False)
         writer = PillowWriter(fps=100)
         anim.save(filename_u, writer=writer)
--- simulation.py
+++ simulation.py
@@ -8,10 +8,10 @@ from Inference import Inference
 N = 200
 model = GrayScott(N)
 parameters = [0.14, 0.06, 0.060, 0.062]
-times = np.arange(0,10*10+10, 10)
+times = np.arange(0,100*10+10, 10)
 values = model.simulate(parameters, times)
 plot = Plot(N)
-plot.animation(values, "animations2.gif")
+#plot.animation(values, "animations2.gif")
 
-#inference = Inference(model, times, values)
-#inference.optimise()
+inference = Inference(model, times, values)
+inference.optimise()
